241a056dba333062d0ce6e090b7f11dda72d01ad
Dmitry Kisler <admin@dkisler.de>
2019-08-18 11:16:35 +0200

    add falcon API

---
+++ hello-world/py/falcon/api.py
@@ -0,0 +1,15 @@
+import falcon
+
+app = falcon.API()
+
+
+class Handler(object):
+
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = 'Hello world!'
+        
+
+handler = Handler()
+
+app.add_route('/', handler)
