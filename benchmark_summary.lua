142a3a4908bce7b9f0f4c0cf327bc7d9e89991cf
Dmitry Kisler <admin@dkisler.de>
2019-08-19 16:31:07 +0200

    add benchmark summary script

---
+++ benchmark_summary.lua
@@ -0,0 +1,16 @@
+done = function(summary, latency, requests)
+   io.write("---\n")
+   io.write("summary_latency\n")
+   io.write("percentile,latency_us\n")
+   for _, p in pairs({0.000001, 5, 25, 50, 75, 95, 99, 99.999999 }) do
+      n = latency:percentile(p)
+      io.write(string.format("%g,%d\n", p, n))
+   end
+   io.write("---\n")
+   io.write("summary_requests\n")
+   io.write("percentile,requests\n")
+	for _, p in pairs({0.000001, 5, 25, 50, 75, 95, 99, 99.999999 }) do
+      n = requests:percentile(p)
+      io.write(string.format("%g,%d\n", p, n))
+   end   
+end
