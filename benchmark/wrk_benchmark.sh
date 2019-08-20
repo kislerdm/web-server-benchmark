2961e41b9bfb9fc9fd271628d5421273d6305eb4
Dmitry Kisler <admin@dkisler.de>
2019-08-20 23:48:33 +0200

    add benchmark runner

---
+++ benchmark/wrk_benchmark.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# benchmark runner
+
+fout=$1 
+url="http://0.0.0.0:4500/"
+script=benchmark_summary.lua
+
+wrk -t1 -c100 -d10s -s ${script} "${url}" > ${fout}
+# wrk -t1 -c200 -d10s "${url}" >> ${fout}
+# wrk -t10 -c100 -d10s "${url}" >> ${fout}
+# wrk -t10 -c200 -d10s "${url}" >> ${fout}
