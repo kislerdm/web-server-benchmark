19c1aa36c4ae8b84289f064ea004efa811a6360f
Dmitry Kisler <admin@dkisler.de>
2019-08-21 11:24:56 +0200

    add settings julia api

--- example/hello-world/julia/genie/api.jl
+++ example/hello-world/julia/genie/api.jl
@@ -3,9 +3,13 @@ import Genie.Router: route
 import Genie.Renderer: json
 
 Genie.config.run_as_server = true
+Genie.config.log_level = :error
+Genie.config.app_env = "prod"
+Genie.config.server_host = "0.0.0.0"
+Genie.config.server_port = 4500
 
 route("/") do
   (:data => "Hello World!") |> json
 end
 
-Genie.startup(4500)
+Genie.startup()
