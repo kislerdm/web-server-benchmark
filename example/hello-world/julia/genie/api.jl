69419696fd57263e4036840bf7b19808f12c581c
Dmitry Kisler <admin@dkisler.de>
2019-08-21 11:28:00 +0200

    fix julia

--- example/hello-world/julia/genie/api.jl
+++ example/hello-world/julia/genie/api.jl
@@ -6,10 +6,9 @@ Genie.config.run_as_server = true
 Genie.config.log_level = :error
 Genie.config.app_env = "prod"
 Genie.config.server_host = "0.0.0.0"
-Genie.config.server_port = 4500
 
 route("/") do
   (:data => "Hello World!") |> json
 end
 
-Genie.startup()
+Genie.startup(4500)
