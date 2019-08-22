786a0017357031c3344b24410e76597468d7b0ed
Dmitry Kisler <admin@dkisler.de>
2019-08-22 14:28:39 +0200

    adjust runners script

--- benchmark/wrk_benchmark.sh
+++ benchmark/wrk_benchmark.sh
@@ -5,7 +5,10 @@ fout=$1
 url="http://0.0.0.0:4500/"
 script=benchmark_summary.lua
 
-wrk -t1 -c100 -d10s -s ${script} "${url}" > ${fout}
-# wrk -t1 -c200 -d10s "${url}" >> ${fout}
-# wrk -t10 -c100 -d10s "${url}" >> ${fout}
-# wrk -t10 -c200 -d10s "${url}" >> ${fout}
+wrk -t1 -c1 -d10s -s ${script} "${url}" > ${fout}
+
+echo "--new-test--">> ${fout}
+wrk -t1 -c100 -d10s -s ${script} "${url}" >> ${fout}
+
+echo "--new-test--">> ${fout}
+wrk -t10 -c100 -d10s -s ${script} "${url}" >> ${fout}
