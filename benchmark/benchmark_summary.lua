765068c4f105d5211863841ee8a019babb498375
Dmitry Kisler <admin@dkisler.de>
2019-08-28 03:13:39 +0200

    adjust benchmark script for min,q1,q2,q3,max only

--- benchmark/benchmark_summary.lua
+++ benchmark/benchmark_summary.lua
@@ -2,14 +2,14 @@ done = function(summary, latency, requests)
    io.write("---\n")
    io.write("summary_latency\n")
    io.write("percentile,latency_us\n")
-   for _, p in pairs({0.000001, 5, 25, 50, 75, 95, 99, 99.999999 }) do
+   for _, p in pairs({0.000001, 25, 50, 75, 99.999999 }) do
       n = latency:percentile(p)
       io.write(string.format("%g,%d\n", p, n))
    end
    io.write("---\n")
    io.write("summary_requests\n")
    io.write("percentile,requests\n")
-	for _, p in pairs({0.000001, 5, 25, 50, 75, 95, 99, 99.999999 }) do
+	for _, p in pairs({0.000001, 25, 50, 75, 99.999999 }) do
       n = requests:percentile(p)
       io.write(string.format("%g,%d\n", p, n))
    end   
