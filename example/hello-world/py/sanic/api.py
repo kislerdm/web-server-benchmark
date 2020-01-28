d8a6fe8109a95279cfd2a1ec582716e0a3e37134
Dmitry Kisler <admin@dkisler.de>
2020-01-28 22:44:48 +0100

    refactor

--- example/hello-world/py/sanic/api.py
+++ example/hello-world/py/sanic/api.py
@@ -9,8 +9,12 @@ app = Sanic(__name__)
 
 @app.route("/")
 async def handler(request):
-    return response.json(body={"data": "Hello world!"})
+    return response.json(body={"data": "Hello world!"},
+                         content_type='application/json',
+                         status=200)
 
 
 if __name__ == "__main__":
     app.run(host=HOST, port=PORT, debug=False)
+
+
